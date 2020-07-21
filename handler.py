144c4b54ba200f1a9cf4ad67bef967c229bca78f
Cameron Beeler <cameron.beeler@gmail.com>
2020-07-20 18:01:55 -0700

    API Get call

--- handler.py
+++ handler.py
@@ -1,16 +1,36 @@
 import json
+import boto3
+import logging
+import os
 
+# function name is get-ingredient
 
 def getIngredient(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        'message': 'CAM:  Go Serverless v1.0! Your function executed successfully!',
+        'input': event
     }
+
+    ingredient = {
+    'name':'hamburger',
+    'UOM':'patty',
+    'quantity': 12,
+    'ranking':8,
+    'useByDate':'072020'
+    }
+
+    # json_ingred = json.loads(ingredient)
+    json_ingred = json.dumps(ingredient)
+    print(json_ingred)
+    print(ingredient.keys())
+    print(ingredient.values())
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        # "body": json_ingred
+        "body": ingredient
     }
+        # "body": ingredient
 
     return response
 
